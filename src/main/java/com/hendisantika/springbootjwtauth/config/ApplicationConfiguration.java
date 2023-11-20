30cbb5ec31495f8fa294b9cfa3ab77765de95379
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:18:41 +0700

    #32 - Add authenticationProvider in ApplicationConfiguration 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
+++ src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -41,4 +43,13 @@ public class ApplicationConfiguration {
         return config.getAuthenticationManager();
     }
 
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
 }
