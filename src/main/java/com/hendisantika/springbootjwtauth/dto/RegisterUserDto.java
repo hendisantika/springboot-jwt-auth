23209191ca042e190cdbf4aa7827602385ea4826
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 10:35:30 +0700

    #5 - Add RegisterUserDto 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootjwtauth/dto/RegisterUserDto.java
@@ -0,0 +1,24 @@
+package com.hendisantika.springbootjwtauth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/20/23
+ * Time: 10:35
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterUserDto {
+    private String email;
+    private String password;
+    private String fullName;
+}
