bfcee00c2e81ce40d809ea5a5aefdbecaad400f8
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:25:03 +0700

    #42 - Add UserController 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/response/LoginResponse.java
+++ src/main/java/com/hendisantika/springbootjwtauth/response/LoginResponse.java
@@ -23,4 +23,15 @@ public class LoginResponse {
     private String token;
 
     private long expiresIn;
+
+    public LoginResponse setToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+
+    public LoginResponse setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+        return this;
+    }
 }
