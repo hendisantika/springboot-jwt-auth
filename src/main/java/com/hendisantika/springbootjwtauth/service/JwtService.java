4f90c32af488ad63937526b0a72b93de60006276
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 10:49:52 +0700

    #19 - Add isTokenValid in JwtService 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/service/JwtService.java
+++ src/main/java/com/hendisantika/springbootjwtauth/service/JwtService.java
@@ -64,4 +64,9 @@ public class JwtService {
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
 }
