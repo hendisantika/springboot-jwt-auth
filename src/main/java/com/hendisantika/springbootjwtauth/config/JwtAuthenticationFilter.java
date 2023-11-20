3d76cb0c15dd2e162c6a4aa12ec924de44fc1e36
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:19:14 +0700

    #33 - Add JwtAuthenticationFilter 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootjwtauth/config/JwtAuthenticationFilter.java
@@ -0,0 +1,27 @@
+package com.hendisantika.springbootjwtauth.config;
+
+import com.hendisantika.springbootjwtauth.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/20/23
+ * Time: 11:18
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final HandlerExceptionResolver handlerExceptionResolver;
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+}
