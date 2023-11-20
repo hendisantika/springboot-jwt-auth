acadccec6b66d6bda9837be48674597c3365391c
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:17:39 +0700

    #30 - Add passwordEncoder in ApplicationConfiguration 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
+++ src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,5 +27,10 @@ public class ApplicationConfiguration {
     UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
