3a5a00630890e6b04634ef0848457d4bd7bd876c
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:20:47 +0700

    #36 - Add securityFilterChain in SecurityConfiguration 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/config/SecurityConfiguration.java
+++ src/main/java/com/hendisantika/springbootjwtauth/config/SecurityConfiguration.java
@@ -1,9 +1,14 @@
 package com.hendisantika.springbootjwtauth.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +27,22 @@ public class SecurityConfiguration {
     private final AuthenticationProvider authenticationProvider;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
 }
