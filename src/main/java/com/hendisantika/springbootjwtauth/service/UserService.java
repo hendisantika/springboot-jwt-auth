deb583a44ee4ddc24453fd6a935a1911c19b1ded
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 10:47:16 +0700

    #11 - Add allUsers in UserService 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/service/UserService.java
+++ src/main/java/com/hendisantika/springbootjwtauth/service/UserService.java
@@ -1,8 +1,12 @@
 package com.hendisantika.springbootjwtauth.service;
 
+import com.hendisantika.springbootjwtauth.entity.User;
 import com.hendisantika.springbootjwtauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +23,9 @@ import org.springframework.stereotype.Service;
 public class UserService {
     private final UserRepository userRepository;
 
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
 }
