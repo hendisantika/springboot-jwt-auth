e673dbfc21f131cf4d1008da4f1e16063e51e320
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 10:30:08 +0700

    #1 - Initial starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootjwtauth/SpringbootJwtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJwtAuthApplication.class, args);
+    }
+
+}
