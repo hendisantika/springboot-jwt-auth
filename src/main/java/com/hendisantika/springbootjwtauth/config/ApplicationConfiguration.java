ce5ecc72db509794012f40ebce360f61e2c1de2a
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:17:25 +0700

    #29 - Add userDetailsService in ApplicationConfiguration 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
+++ src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
@@ -2,7 +2,10 @@ package com.hendisantika.springbootjwtauth.config;
 
 import com.hendisantika.springbootjwtauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -18,4 +21,10 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class ApplicationConfiguration {
     private final UserRepository userRepository;
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
 }
