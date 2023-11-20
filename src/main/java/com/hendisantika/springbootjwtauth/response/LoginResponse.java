b8e0ef65710af989cf5ebf065e7a69f5611d5940
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 10:41:14 +0700

    #9 - Add LoginResponse 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/springbootjwtauth/response/LoginResponse.java
@@ -0,0 +1,26 @@
+package com.hendisantika.springbootjwtauth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jwt-auth
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/20/23
+ * Time: 10:40
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginResponse {
+    private String token;
+
+    private long expiresIn;
+}
