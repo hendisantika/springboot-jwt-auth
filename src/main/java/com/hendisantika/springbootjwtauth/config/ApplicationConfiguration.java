71729b324e844fc29ba064aab87f1844394ef83b
hendisantika <hendisantika@yahoo.co.id>
2023-11-20 11:18:28 +0700

    #31 - Add authenticationManager in ApplicationConfiguration 🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
+++ src/main/java/com/hendisantika/springbootjwtauth/config/ApplicationConfiguration.java
@@ -4,6 +4,8 @@ import com.hendisantika.springbootjwtauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,4 +35,10 @@ public class ApplicationConfiguration {
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
 }
