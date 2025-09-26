db34936099d7736fe58dbeeb12de8d1e0b117f69
hendisantika <hendisantika@yahoo.co.id>
2025-09-26 13:41:54 +0700

    fix: JWT error🫸🌀✏️📗🐧🐳⬆

--- src/main/java/com/hendisantika/springbootjwtauth/service/JwtService.java
+++ src/main/java/com/hendisantika/springbootjwtauth/service/JwtService.java
@@ -84,7 +84,7 @@ public class JwtService {
 
     private Claims extractAllClaims(String token) {
         return Jwts
-                .parserBuilder()
+                .parser()
                 .setSigningKey(getSignInKey())
                 .build()
                 .parseClaimsJws(token)
